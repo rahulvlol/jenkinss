7a6f106828513391772636336b9b44d0b9d5266a
rahulvlol <77204924+rahulvlol@users.noreply.github.com>
2024-11-22 11:37:27 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 25;
+        int num2 = 25;
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
